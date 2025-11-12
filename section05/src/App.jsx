674f7f4e905c5efa563a4d9d8f81e812e89b70f7
DungBeetle0618 <bugreport1@naver.com>
2025-11-12 23:12:00 +0900

    section05 - useState 특징 : value change -> current component reRendering

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 import Header from "./components/header.jsx";
 import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
@@ -7,12 +8,26 @@ import Footer from "./components/Footer.jsx";
 
 
 function App() {
+  useEffect(() => {
+    console.log('effect!')
+  })
+  const state = useState(0)
+  console.log(state)
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState('OFF');
+  console.log(count)
 
   return (
     <>
     <Header/>
     <Main/>
+      <h2 onClick={() => {
+        setLight(light === "OFF" ? "ON" : "OFF")
+      }}>{light}</h2>
      <h1>안녕 리액트!</h1>
+     <button onClick={() => setCount((count)=>count+1)}>
+      {count}
+     </button>
     <Footer/>
     </>
     
