efab46090a07ae977d0b681d7632683e5ea4837a
DungBeetle0618 <bugreport1@naver.com>
2025-11-27 23:15:12 +0900

    49. 5.7) State와 Props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -3,31 +3,20 @@ import { useState, useEffect } from 'react'
 import Header from "./components/header.jsx";
 import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
-
+import Bulb from './components/Bulb.jsx';
+import Counter from './components/Counter.jsx';
 
 
 
 function App() {
-  useEffect(() => {
-    console.log('effect!')
-  })
-  const state = useState(0)
-  console.log(state)
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState('OFF');
-  console.log(count)
+  
 
   return (
     <>
+    <Bulb/>
     <Header/>
     <Main/>
-      <h2 onClick={() => {
-        setLight(light === "OFF" ? "ON" : "OFF")
-      }}>{light}</h2>
-     <h1>안녕 리액트!</h1>
-     <button onClick={() => setCount((count)=>count+1)}>
-      {count}
-     </button>
+    <Counter/>
     <Footer/>
     </>
     
